1f33127a57268d4907d8274ef08c42c5338696e0
Gerd Heber <gheber@hdfgroup.org>
2024-01-02 14:34:01 +0000

    Added a sample SPARQL query.

---
+++ python/data_extraction.py
@@ -0,0 +1,62 @@
+'''
+https://gist.githubusercontent.com/jellevankerk/ea712915e9bb2bd2608c5277981116db/raw/6d89b9dd585f67b1e342416b189dff6c699a79aa/data_extraction.py
+'''
+
+import sys
+import pandas as pd
+from typing import List, Dict
+from SPARQLWrapper import SPARQLWrapper, JSON
+
+class WikiDataQueryResults:
+    """
+    A class that can be used to query data from Wikidata using SPARQL and return the results as a Pandas DataFrame or a list
+    of values for a specific key.
+    """
+    def __init__(self, query: str):
+        """
+        Initializes the WikiDataQueryResults object with a SPARQL query string.
+
+        :param query: A SPARQL query string.
+        """
+        self.user_agent = "WDQS-example Python/%s.%s" % (sys.version_info[0], sys.version_info[1])
+        self.endpoint_url = "https://query.wikidata.org/sparql"
+        self.sparql = SPARQLWrapper(self.endpoint_url, agent=self.user_agent)
+        self.sparql.setQuery(query)
+        self.sparql.setReturnFormat(JSON)
+
+    def __transform2dicts(self, results: List[Dict]) -> List[Dict]:
+        """
+        Helper function to transform SPARQL query results into a list of dictionaries.
+
+        :param results: A list of query results returned by SPARQLWrapper.
+        :return: A list of dictionaries, where each dictionary represents a result row and has keys corresponding to the
+        variables in the SPARQL SELECT clause.
+        """
+        new_results = []
+        for result in results:
+            new_result = {}
+            for key in result:
+                new_result[key] = result[key]['value']
+            new_results.append(new_result)
+        return new_results
+
+    def _load(self) -> List[Dict]:
+        """
+        Helper function that loads the data from Wikidata using the SPARQLWrapper library, and transforms the results into
+        a list of dictionaries.
+
+        :return: A list of dictionaries, where each dictionary represents a result row and has keys corresponding to the
+        variables in the SPARQL SELECT clause.
+        """
+        results = self.sparql.queryAndConvert()['results']['bindings']
+        results = self.__transform2dicts(results)
+        return results
+
+    def load_as_dataframe(self) -> pd.DataFrame:
+        """
+        Executes the SPARQL query and returns the results as a Pandas DataFrame.
+
+        :return: A Pandas DataFrame representing the query results.
+        """
+        results = self._load()
+        return pd.DataFrame.from_dict(results)
